8ea5456f01503f6227f135648e991dfec4e09082
ranjit <patilranjit485@gmail.com>
2023-09-22 15:12:46 +0530

    Logic added

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,13 +3,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainWindow extends JFrame implements ActionListener {
 
-    MenuBar mb;
-    MenuItem m1,m2,m3,m4;
-    Menu mn;
-    MenuShortcut menuShortcut;
+    String fileName;
+    String path;
+    ProcessBuilder Run,Compile,clone;
+
     MainWindow(){
         JFrame f= new JFrame();
         this.setTitle("CodeRunner");
@@ -19,31 +22,8 @@ public class MainWindow extends JFrame implements ActionListener {
         this.setIconImage(logo.getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        this.setLayout(new BorderLayout(3,4));
         this.getContentPane().setBackground(new Color(27,38,44));
 
-        Container main = this.getContentPane();
-        main.setLayout(new BorderLayout(0,0));
-
-        menuShortcut =new MenuShortcut(KeyEvent.VK_X);
-        mn =new Menu("File");
-        mb = new MenuBar();
-        //
-        m1 =new MenuItem("New");
-        m2 =new MenuItem("Open");
-        m3 =new MenuItem("Save As");
-        m4 = new MenuItem("Exit",menuShortcut);
-
-        mn.add(m1);
-        mn.add(m2);
-        mn.add(m3);
-        mn.addSeparator();
-        mb.add(mn);
-
-        Button addNew=new Button("ADD NEW");
-        addNew.setBackground(Color.cyan);
-        addNew.addActionListener(this);
-        main.add(addNew,BorderLayout.SOUTH);
 
         this.setVisible(true);
     }
@@ -51,6 +31,31 @@ public class MainWindow extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("clicked");
+
+        ProcessBuilder Run = new ProcessBuilder(
+                "cmd.exe", "/c", "Git clone https://github.com/ranjit485/codeRunner");
+        Run.redirectErrorStream(true);
+
+        Process p = null;
+
+        try {
+            p = Run.start();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+
+        while (true) {
+            try {
+                line = r.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            if (line == null) { break; }
+            System.out.println(line);
+        }
 
     }
 }
